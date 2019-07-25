8eea31672d5dd7e69bddd112d54be7eb90a743e8
julesbaudet <jules.baudet99@gmail.com>
2019-07-25 17:10:39 +0200

    'actualized version'

--- distributions.py
+++ distributions.py
@@ -3,16 +3,21 @@ from torch.distributions import Exponential
 import numpy as np
 
 class UniformDistrib():
-    def __init__(self, a = 0, b = 1, l = 1): #l is lambda for adversarial training: array
-        self.a, self.b, self.l = a, b, l
+    def __init__(self, mu, sigma, a = 0, b = 1, is_strategic = False): #l is lambda for adversarial training: array
+        self.a, self.b, self.mu, self.sigma = a, b, mu, sigma
+        self.is_strategic = is_strategic
         self.name="uniform"
-        self.cdf=lambda x : (x-a)/(b-a)
-        self.pdf=lambda x : 1/(b-a)
-        self.inverse_virtual_value = lambda x:(x+1)/2 #useless in regretnet
-        self.boost = 2.0 #don't know what that is
-        self.optimal_reserve_price = 0.5 #useless in regretnet
+        # self.cdf=lambda x : (x-a)/(b-a)
+        # self.pdf=lambda x : 1/(b-a)
+        # self.inverse_virtual_value = lambda x:(x+1)/2 #useless in regretnet
+        # self.boost = 2.0 #don't know what that is
+        # self.optimal_reserve_price = 0.5 #useless in regretnet
     def sample(self,size):
-        return self.l * np.random.uniform(self.a, self.b, size)
+        if self.is_strategic: 
+            l = np.random.normal(self.mu, self.sigma, size)
+            return l * np.random.uniform(self.a, self.b, size)
+        else:
+            return np.random.uniform(self.a, self.b, size)
         #return torch.rand(size)
 
 class ExponentialDistrib():
@@ -27,3 +32,14 @@ class ExponentialDistrib():
         return np.random.exponential(1.0,size)
         #m = Exponential(torch.tensor([1.0]))
         #return m.sample(size)
+
+class AdverseDistrib():
+    def __init__(self, distrib, mu = 1, sig = 1):
+        self.mu = mu 
+        self.sig = sig
+        self.distrib = distrib #distrib du player
+    
+    def sample(self, size): #size is number of objects
+        values = self.distrib.sample(size)
+        lam = np.random.normal(loc = mu, scale = sig, size = size) #generates the lambdas from the same distribution
+        return values * self.lambdas
